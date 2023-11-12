055c0c5fd299b3e7b4bbabed69af0759987c7b6a
Moreira <lucas.h.moreira@accenture.com>
2023-11-11 22:33:23 -0300

    função de exclusão criada e inserida no botão touchableopacity

--- AwesomeProject/src/components/Screens/ListScreen.js
+++ AwesomeProject/src/components/Screens/ListScreen.js
@@ -1,53 +1,45 @@
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import React, { useState,useRef } from 'react';
-import { FlatList, Text, View, TextInput, Button, Icon } from 'react-native';
-
-const IconDelete = () => (
-  <Icon
-    name="close"
-    size={20}
-    color="red"
-  />
-);
+import {TouchableOpacity, FlatList, Text, View, TextInput, Button } from 'react-native';
+//import Icon from 'react-native-vector-icons';
+export const ListScreen = ({id0}) => {
 
 
 
-export const ListScreen = () => {
   const [data, setData] = useState([]);
 
   const [value, setValue] = useState('1');
   const [value2, setValue2] = useState('2');
 
+  const deleteItem = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+
 
   let newItem;
-
-  const listRef = useRef();
   const addItem = (text, price) => {
     
     console.log(text,price);
-     newItem = {
+      newItem = {
       id: Math.random().toString(36).substring(7),
-      text,
-      price,
+      text:text,
+      price:price,
       
     };
 
 
     setData([...data, newItem]);
     // Atualiza a lista
-    listRef.current.forceUpdate();
+   // <Icon name="delete" size={24} color="#ff0000" />listRef.current.forceUpdate();
+   
   };
-  const onDelete = (newItem) => {
-    // Remove o item da lista
-    setData(data.filter((item) => item.id !== newItem.id));
-    //console.log(newItem[0].id)
-  };
+
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <FlatList
         data={data}
-        ref={listRef}
+        //ref={listRef}
         renderItem={({ item }) => (
           <View
             style={{
@@ -63,11 +55,17 @@ export const ListScreen = () => {
           >
             <Text style={{ flex: 1, alignItems: 'flex-start' }}>{item.text}</Text>
             <Text style={{ flex: 1, alignItems: 'flex-end' }}>{item.price}</Text>
-            <Button title='' style={{margin:0}}
-             onPress={()=>{onDelete(newItem)}}
-            >
-              {IconDelete}
-              </Button>
+            <TouchableOpacity
+            style={{alignItems: 'center',
+            backgroundColor: 'red',
+            padding: 10,
+            borderRadius:10}}
+      onPress={() => {
+        deleteItem(item.id);
+      }}
+    >
+      
+    </TouchableOpacity>
            
           </View>
         )}
